9261420e555145656438a969adba6d7e5b738302
Gustavo Gonçalves <gustavoantonio01@hotmail.com>
2024-03-21 20:19:30 -0300

    validações de entrada dos parametros no endpoint put

--- IWantApp/Domain/Products/Category.cs
+++ IWantApp/Domain/Products/Category.cs
@@ -4,24 +4,38 @@ namespace IWantApp.Domain.Products
 {
     public class Category : Entity
     {
-        public string Name { get; set; }
-        public bool Active { get; set; }
+        public string Name { get; private set; }
+        public bool Active { get; private set; }
 
         public Category(string name, string createdBy, string editedBy)
         {
-            var contract = new Contract<Category>()
-                .IsNullOrEmpty(name, "Name", "Nome é obrigatório")
-                .IsGreaterOrEqualsThan(name, 3, "Name")
-                .IsNullOrEmpty(createdBy, "CreatedBy", "CreatedBy é obrigatório")
-                .IsNullOrEmpty(editedBy, "EditedBy", "EditedBy é obrigatório");
-
-            AddNotifications(contract);
             Name = name;
             Active = true;
             CreatedBy = createdBy;
             EditedBy = editedBy;
             CreatedOn = DateTime.Now;
             EditedOn = DateTime.Now;
+            Validate();
+
+        }
+
+        private void Validate()
+        {
+            var contract = new Contract<Category>()
+                            .IsNullOrEmpty(Name, "Name", "Nome é obrigatório")
+                            .IsGreaterOrEqualsThan(Name, 3, "Name")
+                            .IsNullOrEmpty(CreatedBy, "CreatedBy", "CreatedBy é obrigatório")
+                            .IsNullOrEmpty(EditedBy, "EditedBy", "EditedBy é obrigatório");
+
+            AddNotifications(contract);
+        }
+
+        public void EditInfo(string name, bool active)
+        {
+            Active = active;
+            Name = name;
+
+            Validate();
         }
     }
 }
--- IWantApp/Endpoints/Categories/CategoryPut.cs
+++ IWantApp/Endpoints/Categories/CategoryPut.cs
@@ -18,8 +18,13 @@ namespace IWantApp.Endpoints.Categories
             {
                 return Results.NotFound();
             }
-            category.Name = categoryRequest.Name;
-            category.Active = categoryRequest.Active;
+
+            category.EditInfo(categoryRequest.Name, categoryRequest.Active);
+
+            if (!category.IsValid)
+            {
+                return Results.ValidationProblem(category.Notifications.ConvertToProblemDetails());
+            }
 
             context.SaveChanges();
             return Results.Ok();
