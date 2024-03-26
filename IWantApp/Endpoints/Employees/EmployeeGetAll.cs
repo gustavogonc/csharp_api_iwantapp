4399f7a3d9b28f1b3ad8194606b2e0a5297542be
Gustavo Gonçalves <gustavoantonio01@hotmail.com>
2024-03-25 21:55:33 -0300

    endpoint para retornar usuários

---
+++ IWantApp/Endpoints/Employees/EmployeeGetAll.cs
@@ -0,0 +1,20 @@
+﻿using IWantApp.Domain.Products;
+using IWantApp.Infra.Data;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace IWantApp.Endpoints.Employees
+{
+    public class EmployeeGetAll
+    {
+        public static string Template => "/employees";
+        public static string[] Methods => new string[] { HttpMethod.Get.ToString() };
+        public static Delegate Handle => Action;
+
+        public static IResult Action(UserManager<IdentityUser> userManager)
+        {
+            var users = userManager.Users.ToList();
+            return Results.Ok(users);
+        }
+    }
+}
