063bbb5f783bc2ab4b75bbe983f597e56e923b0e
Gustavo Gonçalves <gustavoantonio01@hotmail.com>
2024-03-26 20:35:24 -0300

    consulta pagina com dapper

--- IWantApp/Endpoints/Employees/EmployeeGetAll.cs
+++ IWantApp/Endpoints/Employees/EmployeeGetAll.cs
@@ -1,6 +1,8 @@
-﻿using IWantApp.Domain.Products;
+﻿using Dapper;
+using IWantApp.Domain.Products;
 using IWantApp.Infra.Data;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Data.SqlClient;
 using System.Security.Claims;
 
 namespace IWantApp.Endpoints.Employees
@@ -11,18 +13,20 @@ namespace IWantApp.Endpoints.Employees
         public static string[] Methods => new string[] { HttpMethod.Get.ToString() };
         public static Delegate Handle => Action;
 
-        public static IResult Action(int page, int rows, UserManager<IdentityUser> userManager)
+        public static IResult Action(int page, int rows, IConfiguration configuration)
         {
-            var users = userManager.Users.Skip((page - 1) * rows).Take(rows).ToList();
-            var employees = new List<EmployeeResponse>();
-            foreach (var item in users)
-            {
-                var claims = userManager.GetClaimsAsync(item).Result;
-                var claimName = claims.First(c => c.Type == "Name");
-                var userName = claimName != null ? claimName.Value : string.Empty;
-                employees.Add(new EmployeeResponse(item.Email, userName));
-            }
+
+            var db = new SqlConnection(configuration["Database:SqlServer"]);
+            string query = @"select Email, ClaimValue as Name 
+                         from IWantDb.dbo.AspNetUsers u 
+                         inner join IWantDb.dbo.AspNetUserClaims c
+                         on u.id = c.UserId and claimType = 'Name'
+                         order by name
+                         OFFSET (@page - 1) * @rows ROWS FETCH NEXT @rows ROWS ONLY";
+            var employees = db.Query<EmployeeResponse>(query, new {page, rows});
+
             return Results.Ok(employees);
+
         }
     }
 }
