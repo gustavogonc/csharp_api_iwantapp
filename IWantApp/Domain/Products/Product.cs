df19a4da9ed32eddee4245115c871e232e91b01a
Gustavo Gonçalves <gustavoantonio01@hotmail.com>
2024-04-01 21:44:38 -0300

    validações do construtor de produto

--- IWantApp/Domain/Products/Product.cs
+++ IWantApp/Domain/Products/Product.cs
@@ -1,13 +1,49 @@
-﻿namespace IWantApp.Domain.Products
+﻿using Flunt.Validations;
+
+namespace IWantApp.Domain.Products
 {
     public class Product : Entity
     {
+        public Product()
+        {
+            
+        }
+
+        public Product(string name, Category category, string description, bool hasStock, string userId)
+        {
+            Name = name;
+            Category = category;
+            Description = description;
+            HasStock = hasStock;
+
+            CreatedBy = userId;
+            EditedBy = userId;
+            CreatedOn = DateTime.Now;
+            EditedOn = DateTime.Now;
+
+            Validate();
+
+        }
+
         public string Name { get; set; }
         public Guid CategoryId { get; set; }
         public Category Category { get; set; }  
         public string Description { get; set; }
         public bool HasStock { get; set; }
-        public bool Active { get; set; } = true;
+        public bool Active { get; set; }
+
+        private void Validate()
+        {
+            var contract = new Contract<Product>()
+                .IsNotNullOrEmpty(Name, "Name")
+                .IsGreaterOrEqualsThan(Name, 3, "Name")
+                 .IsNotNull(Category, "Category")
+                 .IsNotNullOrEmpty(Description, "Description")
+                 .IsGreaterOrEqualsThan(Description, 3, "Description")
+                 .IsNotNullOrEmpty(CreatedBy, "CreatedBy")
+                 .IsNotNullOrEmpty(EditedBy, "EditedBy");
+            AddNotifications(contract);
+        }
     }
 }
 
--- IWantApp/Endpoints/Products/ProductRequest.cs
+++ IWantApp/Endpoints/Products/ProductRequest.cs
@@ -1,6 +1,4 @@
 ﻿namespace IWantApp.Endpoints.Products
 {
-    public class ProductRequest
-    {
-    }
+    public record ProductRequest(string Name, Guid CategoryId, string Description, bool HasStock, bool Active);
 }
