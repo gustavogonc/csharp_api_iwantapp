73228b6b432a879d012a7004c7581ea3ff0f5b6f
Gustavo Gonçalves <gustavoantonio01@hotmail.com>
2024-04-05 20:51:55 -0300

    validando os parâmetros de entrada do endpoint publico

--- IWantApp/Endpoints/Products/ProductGetShowCase.cs
+++ IWantApp/Endpoints/Products/ProductGetShowCase.cs
@@ -6,31 +6,28 @@
         public static string[] Methods => new string[] { HttpMethod.Get.ToString() };
         public static Delegate Handle => Action;
         [AllowAnonymous]
-        public static async Task<IResult> Action(int? page, int? row, string? orderBy, ApplicationDbContext context)
+        public static async Task<IResult> Action(ApplicationDbContext context, int page = 1, int row = 10, string orderBy = "name")
         {
-            if(page == null)
+           
+            if(row > 10)
             {
-                page = 1;
-            }
-            if (row == null)
-            {
-                row = 1;
-            }
-            if (string.IsNullOrEmpty(orderBy))
-            {
-                orderBy = "name";
+                return Results.Problem(title: "Row with max 10", statusCode: 400)
             }
             var queryBase = context.Products.Include(p => p.Category)
                 .Where(p => p.HasStock && p.Category.Active);
 
-            var queryFilter = queryBase.Skip((page.Value - 1) * row.Value).Take(row.Value);
+            var queryFilter = queryBase.Skip((page - 1) * row).Take(row);
             if(orderBy == "name")
             {
                 queryFilter = queryFilter.OrderBy(p => p.Name);
             }
-            else
+            else if (orderBy == "name")
             {
                 queryFilter = queryFilter.OrderBy(p => p.Price);
+            }
+            else
+            {
+                return Results.Problem(title: "Order by only by price or name", statusCode: 400);
             }
 
             var products = queryFilter.ToList();
