5682b4443604c4521f82d2d1ea18e885cb5fb755
Gustavo Gonçalves <gustavoantonio01@hotmail.com>
2024-04-05 20:40:28 -0300

    paginação do product get show case

--- IWantApp/Endpoints/Products/ProductGetShowCase.cs
+++ IWantApp/Endpoints/Products/ProductGetShowCase.cs
@@ -6,11 +6,35 @@
         public static string[] Methods => new string[] { HttpMethod.Get.ToString() };
         public static Delegate Handle => Action;
         [AllowAnonymous]
-        public static async Task<IResult> Action(ApplicationDbContext context)
+        public static async Task<IResult> Action(int? page, int? row, string? orderBy, ApplicationDbContext context)
         {
-            var products = context.Products.Include(p => p.Category)
-                .Where(p => p.HasStock && p.Category.Active)
-                .OrderBy(p => p.Name).ToList();
+            if(page == null)
+            {
+                page = 1;
+            }
+            if (row == null)
+            {
+                row = 1;
+            }
+            if (string.IsNullOrEmpty(orderBy))
+            {
+                orderBy = "name";
+            }
+            var queryBase = context.Products.Include(p => p.Category)
+                .Where(p => p.HasStock && p.Category.Active);
+
+            var queryFilter = queryBase.Skip((page.Value - 1) * row.Value).Take(row.Value);
+            if(orderBy == "name")
+            {
+                queryFilter = queryFilter.OrderBy(p => p.Name);
+            }
+            else
+            {
+                queryFilter = queryFilter.OrderBy(p => p.Price);
+            }
+
+            var products = queryFilter.ToList();
+
             var results = products.Select(p => new ProductResponse(p.Name, p.Category.Name, p.Description, p.HasStock, p.Price, p.Price, p.Active));
             return Results.Ok(results);
         }
