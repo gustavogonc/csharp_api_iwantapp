293b51d399be1e684745a1042a96b5db630d1bef
Gustavo Gonçalves <gustavoantonio01@hotmail.com>
2024-04-10 21:38:43 -0300

    refatorando código de employee post com reaproveitamento do método criado

--- IWantApp/Endpoints/Employees/EmployeePost.cs
+++ IWantApp/Endpoints/Employees/EmployeePost.cs
@@ -1,4 +1,7 @@
-﻿namespace IWantApp.Endpoints.Employees
+﻿using IWantApp.Domain.Users;
+using static System.Net.WebRequestMethods;
+
+namespace IWantApp.Endpoints.Employees
 {
     public class EmployeePost
     {
@@ -7,17 +10,9 @@
         public static Delegate Handle => Action;
 
         [Authorize(Policy = "EmployeePolicy")]
-        public static async Task<IResult> Action(EmployeeRequest employeeRequest, HttpContext http, UserManager<IdentityUser> userManager)
+        public static async Task<IResult> Action(EmployeeRequest employeeRequest, HttpContext http,UserCreator userCreator)
         {
             var created = http.User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
-            var user = new IdentityUser { UserName = employeeRequest.Email, Email = employeeRequest.Email };
-            var result = await userManager.CreateAsync(user, employeeRequest.Password);
-
-            if (!result.Succeeded)
-            {
-                return Results.ValidationProblem(result.Errors.ConvertToProblemDetails());
-            }
-
             var userClaims = new List<Claim>
             {
                 new Claim("EmployeeCode", employeeRequest.EmployeeCode),
@@ -25,16 +20,38 @@
                 new Claim("CreatedBy", created)
 
             };
-
-            var claimsResult = await userManager.AddClaimsAsync(user, userClaims);
-
-            if (!claimsResult.Succeeded)
+            (IdentityResult identity, string userId) result = await userCreator.Create(employeeRequest.Email, employeeRequest.Password, userClaims);
+            if (!result.identity.Succeeded)
             {
-                return Results.BadRequest(claimsResult.Errors.First());
+                return Results.ValidationProblem(result.identity.Errors.ConvertToProblemDetails());
             }
 
+            //var created = http.User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
+            //var user = new IdentityUser { UserName = employeeRequest.Email, Email = employeeRequest.Email };
+            //var result = await userManager.CreateAsync(user, employeeRequest.Password);
 
-            return Results.Created($"/employees/{user.Id}",user.Id);
+            //if (!result.Succeeded)
+            //{
+            //    return Results.ValidationProblem(result.Errors.ConvertToProblemDetails());
+            //}
+
+            //var userClaims = new List<Claim>
+            //{
+            //    new Claim("EmployeeCode", employeeRequest.EmployeeCode),
+            //    new Claim("Name", employeeRequest.Name),
+            //    new Claim("CreatedBy", created)
+
+            //};
+
+            //var claimsResult = await userManager.AddClaimsAsync(user, userClaims);
+
+            //if (!claimsResult.Succeeded)
+            //{
+            //    return Results.BadRequest(claimsResult.Errors.First());
+            //}
+
+
+            return Results.Created($"/employees/{result.userId}",result.userId);
         }
     }
 }
